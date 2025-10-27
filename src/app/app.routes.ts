ce284c9c37eceda96d17f5fb913b7da5bc6d041f
absiddik <absiddika@gmail.com>
2025-10-28 01:11:20 +0600

    Login and logout with page title change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,17 @@ import { LoginComponent } from './auth/login/login.component';
 import { LayoutComponent } from './layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
 import { OtherComponent } from './pages/other/other.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-    {path:'login', component:LoginComponent},
+    {path:'login', component:LoginComponent, title:'Login'},
     {
         path: '',
         component: LayoutComponent,
+        canActivate: [authGuard],
         children: [
-            {path:'', component:HomeComponent},
-            {path:'other', component:OtherComponent}
+            {path:'', component:HomeComponent, title:'Home'},
+            {path:'other', component:OtherComponent, title:'other'}
         ]
     },
     { path: '**', redirectTo: '' }
